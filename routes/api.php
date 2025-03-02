b78ae9e837c85fbb9771540dfca9b7cd8753380b
Nasreddine Boutouil <nasreddine021@gmail.com>
2025-03-02 12:27:10 +0100

    feat(api): add route name for register endpoint

--- routes/api.php
+++ routes/api.php
@@ -8,4 +8,4 @@ Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
 
-Route::post('/register', [AuthController::class, 'register']);
+Route::post('/register', [AuthController::class, 'register'])->name('auth.register');
