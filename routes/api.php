f3a81b0eb41c332a99cdc18b4adb097e7e62480f
Nasreddine Boutouil <nasreddine021@gmail.com>
2025-03-02 13:32:22 +0100

    fix(routes): correct login route to use LoginController instead of RegisterController

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\LoginController;
 use App\Http\Controllers\RegisterController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
@@ -9,3 +10,6 @@ Route::get('/user', function (Request $request) {
 })->middleware('auth:sanctum');
 
 Route::apiResource('register', RegisterController::class)->only('store');
+
+Route::apiResource('login', LoginController::class)->only('store');
+
