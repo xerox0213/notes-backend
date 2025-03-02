7f5de3fb63dbf6c590b1e73035d3d8d3a7ca4a07
Nasreddine Boutouil <nasreddine021@gmail.com>
2025-03-02 15:50:23 +0100

    refactor(routes): group auth-protected routes under middleware

--- routes/api.php
+++ routes/api.php
@@ -4,11 +4,10 @@ use App\Http\Controllers\AuthController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum');
-
 Route::post('/register', [AuthController::class, 'register'])->name('auth.register');
 Route::post('/login', [AuthController::class, 'login'])->name('auth.login');
-Route::delete('/logout', [AuthController::class, 'logout'])->name('auth.logout');
 
+Route::middleware('auth:sanctum')->group(function() {
+    Route::get('/user', function (Request $request) { return $request->user(); });
+    Route::delete('/logout', [AuthController::class, 'logout'])->name('auth.logout');
+});
