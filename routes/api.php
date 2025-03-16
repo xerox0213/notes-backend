b13387b155de2b321a935d36b25e5277c7cb34e5
Nasreddine Boutouil <nasreddine021@gmail.com>
2025-03-16 20:44:38 +0100

    feat(controllers): add FolderController with index method to retrieve user folders

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\AuthController;
+use App\Http\Controllers\FolderController;
 use App\Http\Middleware\GuestMiddleware;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
@@ -10,7 +11,10 @@ Route::middleware(GuestMiddleware::class)->group(function () {
     Route::post('/login', [AuthController::class, 'login'])->name('auth.login');
 });
 
-Route::middleware('auth:sanctum')->group(function() {
-    Route::get('/user', function (Request $request) { return $request->user(); });
+Route::middleware('auth:sanctum')->group(function () {
+    Route::get('/user', function (Request $request) {
+        return $request->user();
+    });
     Route::delete('/logout', [AuthController::class, 'logout'])->name('auth.logout');
+    Route::apiResource("folders", FolderController::class)->only('index');
 });
