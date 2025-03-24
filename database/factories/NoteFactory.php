90bd0a3e62fa6642bb79156d2b57eae1d8b2ae89
Nasreddine Boutouil <nasreddine021@gmail.com>
2025-03-24 19:43:21 +0100

    feat(NoteFactory): add factory for Note model

---
+++ database/factories/NoteFactory.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Folder;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Note>
+ */
+class NoteFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            'title' =>  fake()->title(),
+            'content' => fake()->text(20),
+            'folder_id' => Folder::factory(),
+        ];
+    }
+}
